5e7e3f82ffddb775aa5dd3408ce0458a697da4d5
Romulo Beninca <rbeninca@gmail.com>
2024-11-06 16:32:16 -0300

    aumento da força do eixo E0, pela elevação da corrente para 1400mah

--- src/kwhprint/Marlin/Configuration_adv.h
+++ src/kwhprint/Marlin/Configuration_adv.h
@@ -2328,7 +2328,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      1400
     #define E0_MICROSTEPS    32
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
